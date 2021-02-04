ad8ca38333e4c58de08590321879e8820f29a0fc
mmalkush <mm.malkush@gmail.com>
2021-02-05 01:20:42 +0200

    prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import {
-  NotificationContainer,
-  NotificationManager,
-} from 'react-notifications';
-import 'react-notifications/lib/notifications.css';
-import OnlineStatusMock from './OnlineStatusMock';
-import './App.css';
+import React from "react";
+import { NotificationContainer, NotificationManager } from "react-notifications";
+import "react-notifications/lib/notifications.css";
+import OnlineStatusMock from "./OnlineStatusMock";
+import "./App.css";
 
 /*
 Feel free to edit this all. If you don't need the HoC, go remove it.
@@ -13,7 +10,7 @@ If you wish to save the state somewhere else, go for it.
 Just keep rendering <OnlineStatusMock />
 */
 
-const withOnlineStatus = WrappedComponent =>
+const withOnlineStatus = (WrappedComponent) =>
   class WithOnlineStatus extends React.Component {
     constructor(props) {
       super(props);
@@ -23,9 +20,7 @@ const withOnlineStatus = WrappedComponent =>
       const { isOnline } = this.state;
       return (
         <>
-          <OnlineStatusMock
-            onIsOnlineChange={isOnline => this.setState({ isOnline })}
-          />
+          <OnlineStatusMock onIsOnlineChange={(isOnline) => this.setState({ isOnline })} />
           <WrappedComponent {...this.props} isOnline={isOnline} />
         </>
       );
@@ -34,15 +29,15 @@ const withOnlineStatus = WrappedComponent =>
 
 class App extends React.Component {
   componentDidUpdate({ isOnline }) {
-    NotificationManager.info(isOnline ? 'Online' : 'Offline');
+    NotificationManager.info(isOnline ? "Online" : "Offline");
   }
 
   render() {
     const { isOnline } = this.props;
     return (
       <>
-        <div className={isOnline ? 'online' : 'offline'}>
-          {isOnline ? 'Online' : 'Offline'}
+        <div className={isOnline ? "online" : "offline"}>
+          {isOnline ? "Online" : "Offline"}
           <NotificationContainer />
         </div>
       </>
